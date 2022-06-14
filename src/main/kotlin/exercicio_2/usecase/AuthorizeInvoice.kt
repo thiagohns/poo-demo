f7bd3bb483f657197b28bd1d6b138a6d20cb8ee6
Thiago Henrique <thiago.henrique@mercadolivre.com>
2022-06-14 09:12:49 -0300

    otimizando import

--- src/main/kotlin/exercicio_2/usecase/AuthorizeInvoice.kt
+++ src/main/kotlin/exercicio_2/usecase/AuthorizeInvoice.kt
@@ -2,10 +2,7 @@ package exercicio_2.usecase
 
 import exercicio_2.Integrador
 import exercicio_2.Invoice
-import exercicio_2.validator.AddressValidator
-import exercicio_2.validator.AmountValidator
 import exercicio_2.validator.InvoiceValidator
-import exercicio_2.validator.IssuerValidator
 
 // Responsavel por regras de negocio
 class AuthorizeInvoice(
