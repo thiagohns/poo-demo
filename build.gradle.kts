74c61703102c7d9fbb3db8096debe9045ec4783b
joseismaelrj <jose.nascimento@mercadolivre.com>
2023-03-01 21:46:22 -0300

    creating formDomain of AddrressesUserEntity class

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//    runtimeOnly("com.h2database:h2") //
+    runtimeOnly("com.h2database:h2") //
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude(module = "spring-boot-starter-tomcat")
     }
